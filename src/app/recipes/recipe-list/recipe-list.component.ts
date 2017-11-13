c8b7596fa730dd680de27574bbfefde76e8c7749
Fred Krauweel <f.krauweel@gmail.com>
2017-11-13 07:05:07 +0100

    tm 47

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -9,7 +9,8 @@ import { Recipe } from '../recipe.model';
 
 export class RecipeListComponent implements OnInit {
    recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'This is simply a test', 'https://c.pxhere.com/photos/9a/90/food_gastronomy_meat_tasty_dinner_restaurant_meal-1376098.jpg!d')
+    new Recipe('A Test Recipe', 'This is simply a test', 'https://c.pxhere.com/photos/9a/90/food_gastronomy_meat_tasty_dinner_restaurant_meal-1376098.jpg!d'),
+    new Recipe('The 2d Test Recipe', 'This is a second simply test', 'http://www.seriouseats.com/recipes/assets_c/2015/01/20150119-pressure-cooker-chicken-stew-food-lab-11-thumb-1500xauto-418088.jpg')
   ];
 
   constructor() { }
